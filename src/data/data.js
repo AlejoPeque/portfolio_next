77ebfe2b5c03d52839b8a06548fa27c492e78121
alejopequeno25 <alejopequeno25@gmail.com>
2023-11-15 18:25:43 +0000

    work experience

--- src/data/data.js
+++ src/data/data.js
@@ -17,33 +17,26 @@ export const navItems = [
   },
 ];
 
-export const projects = [
+export const experiencesData = [
   {
-    name: "Antiria",
-    client: "Digital House",
-    description: "Loremmmmmmmmmmmmmmmmmmm",
-    src: "/Antiria.png",
-    year: 2023,
+    title: "Tester QA",
+    location: "Sistemas Activos SRL",
+    description:
+      "Trabajé como Tester QA en Conciliación Bancaria en Prisma Medios de Pago, enfocado en pruebas unitarias para sistemas Mainframe (COBOL Batch, JCL, VSAM/SAM) utilizando File Manager.",
+    date: "Ago 2021 - Jun 2023",
   },
   {
-    name: "Antiria",
-    client: "Digital House",
-    description: "Loremmmmmmmmmmmmmmmmmmm",
-    src: "/Antiria.png",
-    year: 2023,
+    title: "Full-Stack Developer",
+    location: "Digital House",
+    description:
+      "Participé en el desarrollo Full Stack de una página web con Node.js, Express y MySQL, creando una API específica para el proyecto. También contribuí en la creación de un sitio React que se integró con la API, formando un completo panel de control para productos y usuarios. Esto destaca mis habilidades en proyectos multidisciplinarios.",
+    date: "Mar 2023 - Oct 2023",
   },
   {
-    name: "Antiria",
-    client: "Digital House",
-    description: "Loremmmmmmmmmmmmmmmmmmm",
-    src: "/Antiria.png",
-    year: 2023,
-  },
-  {
-    name: "Antiria",
-    client: "Digital House",
-    description: "Loremmmmmmmmmmmmmmmmmmm",
-    src: "/Antiria.png",
-    year: 2023,
+    title: "Analista QA SSR",
+    location: "Sistemas Activos SRL",
+    description:
+      "Actualmente, soy Analista y Líder QA SSR en el equipo de Conciliación Bancaria en Prisma Medios de Pago. Me dedico al análisis y resolución de problemas en desarrollos e incidentes, además de colaborar en la creación de soluciones para proyectos del Banco Galicia en entornos COBOL Mainframe.",
+    date: "Jun 2023 - Actualidad",
   },
 ];
