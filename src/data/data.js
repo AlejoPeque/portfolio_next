7e54de0c9d66d2dce19f3de9e91aaeef87a83f95
Alejo Pequeño <alejopequeno25@gmail.com>
2023-11-16 16:47:19 -0300

    experience v2

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { CreditCard, Atom, Wallet } from "@phosphor-icons/react";
+
 export const navItems = [
   {
     title: "Inicio",
@@ -19,11 +22,12 @@ export const navItems = [
 
 export const experiencesData = [
   {
-    title: "Tester QA",
+    title: "Analista QA SSR",
     location: "Sistemas Activos SRL",
     description:
-      "Trabajé como Tester QA en Conciliación Bancaria en Prisma Medios de Pago, enfocado en pruebas unitarias para sistemas Mainframe (COBOL Batch, JCL, VSAM/SAM) utilizando File Manager.",
-    date: "Ago 2021 - Jun 2023",
+      "Actualmente, soy Analista y Líder QA SSR en el equipo de Conciliación Bancaria en Prisma Medios de Pago. Me dedico al análisis y resolución de problemas en desarrollos e incidentes, además de colaborar en la creación de soluciones para proyectos del Banco Galicia en entornos COBOL Mainframe.",
+    date: "Jun 2023 - Actualidad",
+    icon: React.createElement(Wallet),
   },
   {
     title: "Full-Stack Developer",
@@ -31,12 +35,14 @@ export const experiencesData = [
     description:
       "Participé en el desarrollo Full Stack de una página web con Node.js, Express y MySQL, creando una API específica para el proyecto. También contribuí en la creación de un sitio React que se integró con la API, formando un completo panel de control para productos y usuarios. Esto destaca mis habilidades en proyectos multidisciplinarios.",
     date: "Mar 2023 - Oct 2023",
+    icon: React.createElement(Atom),
   },
   {
-    title: "Analista QA SSR",
+    title: "Tester QA",
     location: "Sistemas Activos SRL",
     description:
-      "Actualmente, soy Analista y Líder QA SSR en el equipo de Conciliación Bancaria en Prisma Medios de Pago. Me dedico al análisis y resolución de problemas en desarrollos e incidentes, además de colaborar en la creación de soluciones para proyectos del Banco Galicia en entornos COBOL Mainframe.",
-    date: "Jun 2023 - Actualidad",
+      "Trabajé como Tester QA en Conciliación Bancaria en Prisma Medios de Pago, enfocado en pruebas unitarias para sistemas Mainframe (COBOL Batch, JCL, VSAM/SAM) utilizando File Manager.",
+    date: "Ago 2021 - Jun 2023",
+    icon: React.createElement(CreditCard),
   },
 ];
